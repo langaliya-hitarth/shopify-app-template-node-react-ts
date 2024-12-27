9138f83526aab10b63216c38729992d246ab110d
Hitarth Langaliya <dev.hlangaliya@gmail.com>
2024-12-27 23:50:54 +0530

    chore: eslint updates

--- web/src/handlers/webhooks/privacy.ts
+++ web/src/handlers/webhooks/privacy.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DeliveryMethod } from "@shopify/shopify-api";
 import { WebhookHandlersParam } from "@shopify/shopify-app-express";
 
