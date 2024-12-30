5119c284d171f4bdf57acf8c17ffaf26a5eddfa1
Hitarth Langaliya <dev.hlangaliya@gmail.com>
2024-12-30 19:41:39 +0530

    chore: tsconfig updates

--- app/client/src/pages/ExitIframe.tsx
+++ app/client/src/pages/ExitIframe.tsx
@@ -14,15 +14,17 @@ export default function ExitIframe() {
     if (!!app && !!search) {
       const params = new URLSearchParams(search);
       const redirectUri = params.get('redirectUri');
-      const url = new URL(decodeURIComponent(redirectUri));
+      const url = redirectUri ? new URL(decodeURIComponent(redirectUri)) : null;
 
-      if (
-        [location.hostname, 'admin.shopify.com'].includes(url.hostname) ||
-        url.hostname.endsWith('.myshopify.com')
-      ) {
-        window.open(url, '_top');
-      } else {
-        setShowWarning(true);
+      if (url) {
+        if (
+          [location.hostname, 'admin.shopify.com'].includes(url.hostname) ||
+          url.hostname.endsWith('.myshopify.com')
+        ) {
+          window.open(url, '_top');
+        } else {
+          setShowWarning(true);
+        }
       }
     }
   }, [app, search, setShowWarning]);
@@ -32,7 +34,7 @@ export default function ExitIframe() {
       <Layout>
         <Layout.Section>
           <div style={{ marginTop: '100px' }}>
-            <Banner title='Redirecting outside of Shopify' status='warning'>
+            <Banner title='Redirecting outside of Shopify' tone='warning'>
               Apps can only use /exitiframe to reach Shopify or the app itself.
             </Banner>
           </div>
